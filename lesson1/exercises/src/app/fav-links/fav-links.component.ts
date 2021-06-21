6ff086977681b0402d871b862b7fffa37a3d93ad
krystalwhite <krystalsnowhite@gmail.com>
2021-06-20 20:31:39 -0500

    other parts

--- lesson1/exercises/src/app/fav-links/fav-links.component.ts
+++ lesson1/exercises/src/app/fav-links/fav-links.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FavLinksComponent implements OnInit {
 
-  favLinks = ["http://www.nytimes.com", "https://www.merriam-webster.com/word-games/universal-daily-crossword", "http://www.netflix.com"];
+  favLinks = ["http://www.nytimes.com", "https://www.merriam-webster.com/word-games/universal-daily-crossword", "http://www.netflix.com", "https://media.giphy.com/media/1lBKw0J2GYqCyrRWmk/giphy.gif"];
 
   constructor() { }
 
--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -10,6 +10,7 @@ export class FavPhotosComponent implements OnInit {
   image1 = 'https://m.media-amazon.com/images/M/MV5BNWQ1ZmM3MTQtNTVhZC00MWVlLWI5ZjgtYmZiYWQxZjUzZWM0XkEyXkFqcGdeQXVyMzQ2MDI5NjU@._V1_.jpg';
   image2 = 'https://images-na.ssl-images-amazon.com/images/I/71LH4GfAaAL._SY445_.jpg';
   image3 = 'https://m.media-amazon.com/images/M/MV5BMjUxMDEzNTYyN15BMl5BanBnXkFtZTgwMjQyMzg1NzM@._V1_.jpg';
+  image4 = "https://media.giphy.com/media/1lBKw0J2GYqCyrRWmk/giphy.gif";
 
   constructor() { }
 
